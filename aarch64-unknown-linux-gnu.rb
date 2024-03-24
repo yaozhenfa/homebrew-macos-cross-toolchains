95b94fbb2caa28c48d6916eda3f6fac2d84d8f03
Alone <yaozhenfa@users.noreply.github.com>
2024-03-24 10:59:17 +0800

    Update aarch64-unknown-linux-gnu.rb

--- aarch64-unknown-linux-gnu.rb
+++ aarch64-unknown-linux-gnu.rb
@@ -13,8 +13,8 @@ class Aarch64UnknownLinuxGnu < Formula
     url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v13.2.0/aarch64-unknown-linux-gnu-aarch64-darwin.tar.gz"
     sha256 "a87669a9df908d8d8859849a0f9fc0fb287561a4e449c21dade10663d42d2ccb"
   else
-    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v13.2.0/aarch64-unknown-linux-gnu-x86_64-darwin.tar.gz"
-    sha256 "6979291e34064583ac8b12a8b6b99ec6829caf22f47bcb68b646365ec9e24690"
+    url "https://github.com/yaozhenfa/homebrew-macos-cross-toolchains/releases/download/v1.0/aarch64-unknown-linux-gnu-x86_64-darwin.tar"
+    sha256 "aa12838587d1a1bf97428b5a42cad9fe7b543b75f934efa8c1bceaedc13fc1bc"
   end
 
   def install
